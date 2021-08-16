d26b121a9c13884089341ee0ea39b8cf6f853bef
bardenha <bardenha@gmail.com>
2021-08-16 22:54:31 +0100

    CSS configs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div className="App">
-      <p>Weather App</p>
+    <div className="App p-6 h-screen bg-gray-100">
+      <div className="p-6 max-w-sm mx-auto bg-blue-600 shadow-md text-center rounded-md">
+        <p className="text-2xl text-white font-fredoka">Weather App</p>
+      </div>
     </div>
   );
 }
