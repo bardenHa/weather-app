2ad505206bfec51796609aaedf042cf541b3e22f
bardenha <bardenha@gmail.com>
2021-08-28 23:49:29 +0100

    Added credits component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-function App() {
-  return (
-    <div className="App p-6 h-screen bg-gray-100">
-      <div className="p-6 max-w-sm mx-auto bg-blue-600 shadow-md text-center rounded-md container">
-        <p className="text-5xl text-white font-title">Weather App</p>
+import { Component } from "react";
+import Credit from "./components/Credit";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App p-6 h-screen bg-gray-100">
+        <div className="py-6 max-w-2xl mx-auto bg-blue-600 shadow-md text-center rounded-md">
+          <p className="text-5xl text-white font-title">Weather App</p>
+        </div>
+        <Credit />
       </div>
-      <div className="text-center">
-        Icons made by{" "}
-        <a href={"https://www.freepik.com"} title="Freepik">
-          Freepik
-        </a>{" "}
-        from{" "}
-        <a href={"https://www.flaticon.com/"} title="Flaticon">
-          www.flaticon.com
-        </a>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Credit.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Credit extends Component {
+  render() {
+    return (
+      <div className="text-center">
+        Icons made by{" "}
+        <a href={"https://www.freepik.com"} title="Freepik">
+          Freepik
+        </a>{" "}
+        from{" "}
+        <a href={"https://www.flaticon.com/"} title="Flaticon">
+          www.flaticon.com
+        </a>
+      </div>
+    );
+  }
+}
+
+export default Credit;
