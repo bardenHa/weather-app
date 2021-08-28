09431986ffab693cd35f48a6a4e87e56fb2ee304
bardenha <bardenha@gmail.com>
2021-08-28 22:48:56 +0100

    Added title font

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 function App() {
   return (
     <div className="App p-6 h-screen bg-gray-100">
-      <div className="p-6 max-w-sm mx-auto bg-blue-600 shadow-md text-center rounded-md">
-        <p className="text-2xl text-white font-fredoka">Weather App</p>
+      <div className="p-6 max-w-sm mx-auto bg-blue-600 shadow-md text-center rounded-md container">
+        <p className="text-5xl text-white font-title">Weather App</p>
+      </div>
+      <div className="text-center">
+        Icons made by{" "}
+        <a href={"https://www.freepik.com"} title="Freepik">
+          Freepik
+        </a>{" "}
+        from{" "}
+        <a href={"https://www.flaticon.com/"} title="Flaticon">
+          www.flaticon.com
+        </a>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {},
     fontFamily: {
-      Fredoka: "Fredoka One",
+      title: ["Lobster"],
     },
   },
   variants: {
