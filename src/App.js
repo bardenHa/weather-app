9504f2b18c72c649c6fd84e141ca6a42d948f3d5
bardenha <bardenha@gmail.com>
2021-08-29 18:32:57 +0100

    Result form file setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import Credit from "./components/Credit";
 import Form from "./components/Form";
+import ResultForm from "./components/ResultForm";
 
 class App extends Component {
   render() {
@@ -8,7 +9,8 @@ class App extends Component {
       <div className="App p-6 h-screen bg-gray-100">
         <div className="py-6 px-2 max-w-2xl mx-auto bg-blue-600 shadow-md text-center rounded-md">
           <p className="text-5xl text-white font-title pb-6">Weather App</p>
-          <Form />
+          {/*<Form /> */}
+          <ResultForm />
         </div>
         <Credit />
       </div>
