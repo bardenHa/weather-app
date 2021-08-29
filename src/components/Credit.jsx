f4dd025fefbd12207df6c42d1af18d6dcc3eca20
bardenha <bardenha@gmail.com>
2021-08-29 18:40:08 +0100

    Added openweather api to Credits

--- src/components/Credit.jsx
+++ src/components/Credit.jsx
@@ -3,15 +3,23 @@ import React, { Component } from "react";
 class Credit extends Component {
   render() {
     return (
-      <div className="text-center my-6">
-        Icons made by{" "}
-        <a href={"https://www.freepik.com"} title="Freepik">
-          Freepik
-        </a>{" "}
-        from{" "}
-        <a href={"https://www.flaticon.com/"} title="Flaticon">
-          www.flaticon.com
-        </a>
+      <div>
+        <div className="text-center my-6">
+          Powered with{" "}
+          <a href={"https://openweathermap.org/api"} title="OpenWeather API">
+            OpenWeather API
+          </a>
+        </div>
+        <div className="text-center my-6">
+          Icons made by{" "}
+          <a href={"https://www.freepik.com"} title="Freepik">
+            Freepik
+          </a>{" "}
+          from{" "}
+          <a href={"https://www.flaticon.com/"} title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
       </div>
     );
   }
