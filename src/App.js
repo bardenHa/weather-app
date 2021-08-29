f08d577c6798675efcdc35f9d0abc00321582f6d
bardenha <bardenha@gmail.com>
2021-08-29 18:43:04 +0100

    Padding change for lg screens

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ResultForm from "./components/ResultForm";
 class App extends Component {
   render() {
     return (
-      <div className="App p-6 h-screen bg-gray-100">
+      <div className="App p-6 lg:p-24 h-screen bg-gray-100">
         <div className="py-6 px-2 max-w-2xl mx-auto bg-blue-600 shadow-md text-center rounded-md">
           <p className="text-5xl text-white font-title pb-6">Weather App</p>
           {/*<Form /> */}
