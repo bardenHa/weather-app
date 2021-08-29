28e4a8d8e7ef3aca3d52c81fa23f8a7ae216cf86
bardenha <bardenha@gmail.com>
2021-08-29 20:03:46 +0100

    Result form styling update

--- src/components/ResultForm.jsx
+++ src/components/ResultForm.jsx
@@ -3,18 +3,27 @@ import { Component } from "react";
 class ResultForm extends Component {
   render() {
     return (
-      <form className="w-full max-w-sm bg-white mx-auto mx-2 py-8 rounded-md">
-        <div className="flex items-center mx-8 space-x-3">
-          <input
-            className="w-full border-2 text-black bg-gray-200 rounded-md p-0.5 px-1.5 focus:outline-none focus:ring-2 focus:ring-opacity-50 focus:ring-blue-400 focus:border-blue-400"
-            type="text"
-            placeholder="What city?"
-          />
-          <button className="font-regular flex-shrink-0 py-1 px-2 bg-blue-500 rounded-md text-white focus:outline-none hover:bg-blue-600">
-            Search
-          </button>
+      <div className="w-full max-w-sm bg-white mx-auto mx-2 py-8 rounded-md text-regular">
+        <div className="mx-8 mt-4">
+          <h1 className="text-left text-2xl font-bold">London, GB</h1>
         </div>
-      </form>
+        <div className="mx-8 mt-4">
+          <table className="w-full table-fixed border-gray-200 border-collapse">
+            <tr className="border-none">
+              <th className="w-1/4 py-2 border-b border-gray-300">Low</th>
+              <td className="py-2 px-4 border-b border-gray-300 text-right">
+                16.4 C
+              </td>
+            </tr>
+            <tr className="border-none">
+              <th className="w-1/4 py-2 border-b border-gray-300">High</th>
+              <td className="py-2 px-4 border-b border-gray-300 text-right">
+                36.6 C
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
     );
   }
 }
