db6ddc02abe190dc6b03cbc8794e9334a0636f26
bardenha <bardenha@gmail.com>
2021-08-30 18:29:35 +0100

    style: changed bottom padding

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const App = () => {
 
   return (
     <div className="App p-6 lg:p-24 h-screen bg-gray-100">
-      <div className="py-6 px-2 max-w-2xl mx-auto bg-blue-600 shadow-md text-center rounded-md">
+      <div className="pt-6 pb-12 px-2 max-w-2xl mx-auto bg-blue-600 shadow-md text-center rounded-md">
         <p className="text-5xl text-white font-title pb-6">Weather App</p>
         {component}
       </div>
