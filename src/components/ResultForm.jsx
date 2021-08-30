90a821b7207d2413a93d60d0bf1cc1fc112bf684
bardenha <bardenha@gmail.com>
2021-08-30 14:11:35 +0100

    style: added required weather rows

--- src/components/ResultForm.jsx
+++ src/components/ResultForm.jsx
@@ -10,19 +10,59 @@ class ResultForm extends Component {
         <div className="mx-8 mt-4">
           <table className="w-full table-fixed border-gray-200 border-collapse">
             <tr className="border-none">
-              <th className="w-1/4 py-2 px-4 border-b border-gray-300 text-left">
+              <th className="py-2 px-4 border-b border-gray-300 text-left">
                 Low
               </th>
               <td className="py-2 px-4 border-b border-gray-300 text-right">
-                16.4 C
+                16.4°C
               </td>
             </tr>
             <tr className="border-none">
-              <th className="w-1/4 py-2 px-4 border-b border-gray-300 text-left">
-                Highest
+              <th className="py-2 px-4 border-b border-gray-300 text-left">
+                High
               </th>
               <td className="py-2 px-4 border-b border-gray-300 text-right">
-                36.6 C
+                36.6°C
+              </td>
+            </tr>
+            <tr className="border-none">
+              <th className="py-2 px-4 border-b border-gray-300 text-left">
+                Feels like
+              </th>
+              <td className="py-2 px-4 border-b border-gray-300 text-right">
+                36.6°C
+              </td>
+            </tr>
+            <tr className="border-none">
+              <th className="py-2 px-4 border-b border-gray-300 text-left">
+                Wind speed
+              </th>
+              <td className="py-2 px-4 border-b border-gray-300 text-right">
+                12 m/s
+              </td>
+            </tr>
+            <tr className="border-none">
+              <th className="py-2 px-4 border-b border-gray-300 text-left">
+                Pressure
+              </th>
+              <td className="py-2 px-4 border-b border-gray-300 text-right">
+                1000 hPa
+              </td>
+            </tr>
+            <tr className="border-none">
+              <th className="py-2 px-4 border-b border-gray-300 text-left">
+                Humidity
+              </th>
+              <td className="py-2 px-4 border-b border-gray-300 text-right">
+                12%
+              </td>
+            </tr>
+            <tr className="border-none">
+              <th className="py-2 px-4 border-b border-gray-300 text-left">
+                Cloudiness
+              </th>
+              <td className="py-2 px-4 border-b border-gray-300 text-right">
+                12%
               </td>
             </tr>
           </table>
