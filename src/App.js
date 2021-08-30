a45219bc19bed07dac24e479e6081aad7d4128d3
bardenha <bardenha@gmail.com>
2021-08-30 14:54:57 +0100

    feat: Added 404 page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import Credit from "./components/Credit";
+import ErrorPage from "./components/ErrorPage";
 import Form from "./components/Form";
 import ResultForm from "./components/ResultForm";
 
@@ -9,8 +10,8 @@ class App extends Component {
       <div className="App p-6 lg:p-24 h-screen bg-gray-100">
         <div className="py-6 px-2 max-w-2xl mx-auto bg-blue-600 shadow-md text-center rounded-md">
           <p className="text-5xl text-white font-title pb-6">Weather App</p>
-          {/*<Form /> */}
-          <ResultForm />
+          <Form />
+          <ErrorPage />
         </div>
         <Credit />
       </div>
